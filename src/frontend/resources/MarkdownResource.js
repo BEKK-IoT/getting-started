1f6eef6d0604aa9131409060ef4b15ab6fef06e3
Vegard Veiset <veiset@gmail.com>
2015-09-27 13:40:22 +0200

    Update tasks.

--- src/frontend/resources/MarkdownResource.js
+++ src/frontend/resources/MarkdownResource.js
@@ -1,7 +1,23 @@
 import Q from 'q';
 import request from 'superagent';
 import Remarkable from 'remarkable';
-const md = new Remarkable();
+import hljs from 'highlight.js';
+
+const md = new Remarkable({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) {}
+    }
+ 
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) {}
+ 
+    return ''; // use external default escaping 
+  }
+});
 
 function handleResponse(deferred) {
     return (err, res) => err ? deferred.reject(err) : deferred.resolve(md.render(res.text));
--- src/text/arduino/intro.md
+++ src/text/arduino/intro.md
@@ -1,5 +1,5 @@
 # Arduino
-
+ddd
 Arduino/Genuino UNO is the best board to get started with electronics and coding. If this is your first experience with Arduino, the UNO is the most robust board you can start playing with. The UNO is the most used and documented board of the whole Arduino family.
 
 ![Arduno](static/img/gadget/arduino.png)
