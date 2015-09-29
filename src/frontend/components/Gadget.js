999dce3ef26d322dd49f431d7a8444dc6a8a0b0e
Vegard Veiset <veiset@gmail.com>
2015-09-29 22:50:30 +0200

    Add token finder for photon devices.

--- src/frontend/components/Gadget.js
+++ src/frontend/components/Gadget.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Article from './Article';
 import Tasks from './Tasks';
 import InstallScript from './InstallScript';
+import PhotoAccessToken from './PhotoAccessToken';
 
 module.exports = React.createClass({
     render() {
@@ -9,6 +10,7 @@ module.exports = React.createClass({
         const install = this.props.params.gadget + '/install';
         const example = this.props.params.gadget + '/example';
         const gadgetTasks = this.props.params.gadget + '/tasks';
+        const getToken = this.props.params.gadget === 'photon' ? <PhotoAccessToken/> : null;
         return (
             <div className="app-wrapper gadget">
                 <div>
@@ -17,6 +19,7 @@ module.exports = React.createClass({
                 <div>
                     <Article article={install}/>
                     <InstallScript gadget={this.props.params.gadget} />
+                    {getToken}
                 </div>
                 <div>
                     <Article article='run' />
