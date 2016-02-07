659aa20fb085099416a4b2ce20d96e4c1acff5af
Vegard Veiset <veiset@gmail.com>
2016-02-07 18:09:37 +0100

    Update task overview.

--- src/frontend/constants/AppConstants.js
+++ src/frontend/constants/AppConstants.js
@@ -8,27 +8,39 @@ const AppConstants = {
     		tasks: [
 				{
 					path: 'arduino/blinking_led',
-					name: '1: Guide - Blinking leds'
+					name: '1: Guide - Blinking LEDs'
 				},
 				{
 					path: 'arduino/firebase-lamp',
 					name: '2: Guide - Controlling the lamp'
 				},
 				{
-					path: 'arduino/potentiometer',
-					name: '3: Intro - Potentiometer'
+					path: 'arduino/firebase-x',
+					name: '3: Turn on a LED with a button'
 				},
 				{
 					path: 'arduino/task4',
-					name: '4: Something'
+					name: '4: Intro - Light sensor'
 				},
 				{
 					path: 'arduino/task5',
-					name: '5: Something else'
+					name: '5: Intro - Read from Firebase'
 				},
 				{
 					path: 'arduino/task6',
-					name: '6: Good news'
+					name: '6: Control a LED with the Light sensor'
+				},
+				{
+					path: 'arduino/firebase-z',
+					name: '7: Neopixels, multi colored LEDs'
+				},
+				{
+					path: 'arduino/firebase-g',
+					name: '8: Win the game'
+				},
+				{
+					path: 'arduino/firebase-e',
+					name: '9: Challenge - Aim for the blink'
 				}]
 
     	}
