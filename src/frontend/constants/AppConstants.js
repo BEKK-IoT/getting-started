7c9a1cd9b9f9eeb11bcdc61b484d66dbd2b0420c
Vegard Veiset <veiset@gmail.com>
2016-02-07 16:20:26 +0100

    Add custom name to tasks.

--- src/frontend/constants/AppConstants.js
+++ src/frontend/constants/AppConstants.js
@@ -5,7 +5,26 @@ const AppConstants = {
     		name: 'Arduino',
     		url: 'https://github.com/BEKK-IoT/arduino-uno',
     		git: 'https://github.com/BEKK-IoT/arduino-uno.git',
-    		tasks: ['arduino/blinking_led', 'arduino/button', 'arduino/potentiometer', 'arduino/task4', 'arduino/task5', 'arduino/task6']
+    		tasks: [
+				{
+					path: 'arduino/blinking_led',
+					name: 'Blinking leds'
+				}, {
+					path: 'arduino/button',
+					name: 'Button'
+				}, {
+					path: 'arduino/potentiometer',
+					name: 'Potentiometer'
+				}, {
+					path: 'arduino/task4',
+					name: 'Task 4 - '
+				}, {
+					path: 'arduino/task5',
+					name: 'Task 5 - '
+				}, {
+					path: 'arduino/task6',
+					name: 'Task 6'
+				}]
 
     	}
     },
