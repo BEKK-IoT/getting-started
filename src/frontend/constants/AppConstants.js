db7d74d598a6c5ebdef4ea9087242c770e424320
Birgitte Haavardsholm <birgitte__89@hotmail.com>
2016-02-07 16:10:26 +0100

    fikset liten mergefeil

--- src/frontend/constants/AppConstants.js
+++ src/frontend/constants/AppConstants.js
@@ -5,7 +5,7 @@ const AppConstants = {
     		name: 'Arduino',
     		url: 'https://github.com/BEKK-IoT/arduino-uno',
     		git: 'https://github.com/BEKK-IoT/arduino-uno.git',
-    		tasks: ['arduino/led', 'arduino/button', 'arduino/potentiometer', 'arduino/task4', 'arduino/task5', 'arduino/task6']
+    		tasks: ['arduino/blinking_led', 'arduino/button', 'arduino/potentiometer', 'arduino/task4', 'arduino/task5', 'arduino/task6']
 
     	}
     },
